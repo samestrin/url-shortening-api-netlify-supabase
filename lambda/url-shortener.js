e6d6baafe7f2663ad3ad375f9b7f6ef190de9351
Sam Estrin <sam.estrin@gmail.com>
2024-05-11 11:43:23 -0700

    Update

--- lambda/url-shortener.js
+++ lambda/url-shortener.js
@@ -20,6 +20,7 @@ shortid.seed(Math.random().toString(36).slice(2)); // Seed the generator for bet
 
 exports.handler = async (event) => {
   try {
+    console.log(event);
     if (event.httpMethod === "POST" && event.path === "/shorten") {
       if (!event.body) {
         return {
